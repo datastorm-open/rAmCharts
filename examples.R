2df9268d23f3e8e8609b20c5ba92d600c14cef6a
bthieurmel <bthieurmel@gmail.com>
2015-09-02 12:33:30 +0200

    add cutom listeners legend example

--- examples.R
+++ examples.R
@@ -21,7 +21,36 @@ amPieChart(theme ="dark", valueField = "value", titleField = "key", legend = leg
 ) %>>% setDataProvider(data.frame(key = c("FR", "US"), value = c(20,10))
 ) %>>% plot()
 
+### custom legend listener, using chart method
+df <- data.frame(binf=c(1,2,3,4,5),bsup = c(3,4,5,6,7), value = c(2,3,4,5,6),heure = c("9h","10h","11h","12h","13h"))
+titre <- "essai"
 
+legend <- amLegend(equalWidths=FALSE, position="bottom", valueAlign="left", valueWidth=100
+) %>>% addListener("hideItem" , 'function(event){
+  var id = event.dataItem.id;
+  event.chart.hideGraph(event.chart.getGraphById(id + "_from"));
+  event.chart.hideGraph(event.chart.getGraphById(id + "_to"));
+}') %>>% addListener("showItem" , 'function(event){
+  var id = event.dataItem.id;
+  event.chart.showGraph(event.chart.getGraphById(id + "_from"));
+  event.chart.showGraph(event.chart.getGraphById(id + "_to"));
+}')
+
+
+amSerialChart(legend = legend)%>>%
+  setDataProvider(df) %>>%
+  setProperties(type="serial",addClassNames=TRUE,theme="light",autoMargins=TRUE,startDuration=0.5,categoryField="heure",export = list(enabled=TRUE))%>>%
+  setBalloon(adjustBorderColor=FALSE,horizontalPadding=10,verticalPadding=8,color="#ffffff")%>>%
+  addValueAxes(axisAlpha=0,position="left")%>>%
+  setGraphs(list(
+    amGraph(id = "2014_from", lineAlpha = 0, valueField = "binf", showBalloon = FALSE, visibleInLegend = FALSE),
+    amGraph(id = "2014_to", lineAlpha = 0, fillAlphas = 0.2, fillToGraph = "2014_from",valueField = "bsup", 
+            showBalloon = FALSE, visibleInLegend = FALSE),
+    amGraph(id = "2014", valueField = "value")))%>>%
+  setCategoryAxis(gridPosition="start",axisAlpha=0,tickLength=0)%>>%
+  setChartCursor(cursorAlpha=1)%>>%
+  addTitle(text=titre)%>>%
+  plot
 
 ### amRadarChart
 amRadarChart(theme = "chalk", startDuration = 1, categoryField = "attribute"
