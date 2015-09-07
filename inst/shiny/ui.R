4547a23424798681dbcc16a480a509ca7d861fbb
djaiff <petit.jeffery@gmail.com>
2015-09-07 14:28:13 +0200

    new interface

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -9,20 +9,18 @@ library(shiny)
 library(rAmCharts)
 
 shinyUI(fluidPage(
-  
   # Application title
   titlePanel("Old Faithful Geyser Data"),
   
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
+  fluidRow(
+    column(
+      width = 6,
       rAmCharts::amChartsOutput("radar", type = "radar"),
       rAmCharts::amChartsOutput("radar2", type = "radar"),
-      rAmCharts::amChartsOutput("pie", type = "pie"),
+      rAmCharts::amChartsOutput("pie", type = "pie")
+    ),
+    column(
+      width = 6,
       rAmCharts::amChartsOutput("serial", type = "serial"),
       rAmCharts::amChartsOutput("drillColumnChart1", type = "drill"),
       rAmCharts::amChartsOutput("drillColumnChart2", type = "drill")
