ff432026e9aca2a6487af43bf7e3544d3d6ea342
djaiff <petit.jeffery@gmail.com>
2015-09-16 09:53:19 +0200

    Add line for removing comments

--- R/startupMessage.R
+++ R/startupMessage.R
@@ -2,4 +2,6 @@
   packageStartupMessage("rAmCharts is still in developement")
   packageStartupMessage("See profile 'DataKnowledge' on GitHub for the latest version")
   packageStartupMessage("or run directly devtools::install_github('dataKnowledge/rAmCharts')")
+  packageStartupMessage("\nRemove this message with the following command line:")
+  packageStartupMessage("\tsuppressPackageStartupMessages(library(rAmCharts))")
 }
