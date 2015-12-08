dc08eba0a58bd4cce2406003ac37923280e17dd4
Elena <elena.salette@datak.fr>
2015-12-08 15:51:13 +0100

    add parameters to pie chart

--- R/amPie.R
+++ R/amPie.R
@@ -8,16 +8,18 @@
 #' @param legend \code{boolean} TRUE to display legend.
 #' @param legend_side \code{character} either "left" or "right" if legend is set to TRUE
 #' @param show_values \code{boolean} TRUE to display values.
+#' @param third_dim \code{boolean} if TRUE, chart is displayed in 3D
+#' @param inner_radius \code{numeric} value between 0 and 100
 #' @examples
 #' amPie(data = data.frame(label = c("Facebook", "Twitter", "LinkedIn", "Google+", 
 #'                                        "Pinterest"),
 #'                               value = c(38, 25, 15, 14, 8), 
-#'                               stringsAsFactors = FALSE), legend = TRUE,
-#'             main = "Favourite social media", show_values = TRUE)
+#'                               stringsAsFactors = FALSE),
+#'             main = "Favourite social media")
 #' @export
 
 amPie <- function(data, main = "", mainSize = 15, legend = FALSE, legend_side = NULL,
-                  show_values = TRUE) {
+                  show_values = TRUE, third_dim = FALSE, inner_radius = 0) {
   
   if(!is.data.frame(data) | !any(c("label", "value") %in% colnames(data))) {
     stop ("data must be a data frame which at least the columns 'label' (character),
@@ -52,6 +54,16 @@ amPie <- function(data, main = "", mainSize = 15, legend = FALSE, legend_side = 
     stop("show_values must be logical")
   } else {}
   
+  if(!is.logical(third_dim)) {
+    stop("third_dim must be logical")
+  } else {}
+  
+  if(!is.numeric(inner_radius)) {
+    stop("inner_radius must be a numeric")
+  } else if(inner_radius < 0 | inner_radius > 100) {
+    stop("inner_radius must be a numeric between 0 and 100")
+  }
+  
   if(!"color" %in% colnames(data)) {
     vec_col <- tolower(head(rep(c("#67b7dc", "#fdd400", "#84b761", "#cc4748", 
                                   "#cd82ad", "#2f4074", "#448e4d", "#b7b83f", 
@@ -59,12 +71,19 @@ amPie <- function(data, main = "", mainSize = 15, legend = FALSE, legend_side = 
                             nrow(data)))
     data$color <- vec_col
   }
-
-  res <- pipeR::pipeline( 
-    amPieChart(dataProvider = data, valueField = "value", titleField = "label",
-               colorField = "color", labelsEnabled = show_values),
-    addTitle(text = main, size = mainSize)
-  )
+  
+  if(third_dim) {
+    res <- amPieChart(dataProvider = data, valueField = "value", titleField = "label",
+               colorField = "color", labelsEnabled = show_values, depth3D = 10,
+               angle = 15, innerRadius = paste(inner_radius,"%"))
+  } else {
+    res <- amPieChart(dataProvider = data, valueField = "value", titleField = "label",
+                      colorField = "color", labelsEnabled = show_values,
+                      innerRadius = paste(inner_radius,"%"))
+  }
+  
+  res <- addTitle(res, text = main, size = mainSize)
+  
   if(legend) {
     res <- setLegend(res, position = ifelse(is.null(legend_side), 'right',legend_side))
   }
