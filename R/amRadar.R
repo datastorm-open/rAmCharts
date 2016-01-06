640fdf735b77d02455df5e2a0e0077b61635d76b
Titouan Robert <tr@datak.fr>
2016-01-06 10:09:28 +0100

    add export and rd

--- R/amRadar.R
+++ R/amRadar.R
@@ -13,7 +13,7 @@
 #' @param pch : symbols
 #' 
 #' 
-#' @exemple
+#' @example
 #' require(pipeR)
 #' data <- data.frame(label = c("A", "Z", "E", "R", "T"), Product1 = c(1, 2, 3, 4, 2), Product2 = c(2, 8, 1, 1, 0),Product3 = c(1,1,2,2,4))
 #' amRadar(data)
