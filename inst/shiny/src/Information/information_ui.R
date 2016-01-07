d5cdabc9484a69d050072ce925504ca88796b6a3
Titouan Robert <tr@datak.fr>
2016-01-07 18:01:08 +0100

    Eng
    Eng

--- inst/shiny/src/Information/information_ui.R
+++ inst/shiny/src/Information/information_ui.R
@@ -1,15 +1,17 @@
 shinydashboard::tabItem(
   tabName = "information",
   h1("rAmCharts"),
-  p(strong("rAmChart "),"is a graphic package. It was based on ",   a("amcharts",href='https://www.amcharts.com/amCharts') , "javascript library."),
+  p(strong("rAmChart "),"is a graphic package. It is based on the javascript library", a("amcharts",href='https://www.amcharts.com/amCharts'), "."),
   p("Development is done in two steps :"),
   
   tags$ul(
-    tags$li("Integration of all amCharts library function to construct json object. These functions are difficult to use but they offer contruction possibilities very varied graphics."), 
-    tags$li("Adaptation of complex construction way to simple R functions. They are esay to use and options of base R'graphics are keep up. They are not exthautives.")
+    tags$li("Integration of all amCharts library function to construct json object.
+            These functions are difficult to use but they offer contruction possibilities very varied graphics."), 
+    tags$li("Adaptation of complex construction way to simple R functions. They are esay to use and options
+            of base R'graphics are keep up. They are not exthautives.")
   ),
-  p("All wep page named 'Complexe Function' use the first method"),
-  p("All wep page named 'Simple Function' use the second method")
+  p("All wep page named 'Complexe Function' use the first method."),
+  p("All wep page named 'Simple Function' use the second method.")
   
 
 )
