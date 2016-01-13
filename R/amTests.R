317adb34f97ece4892acf4a72b0a2de40cf9f8c2
Titouan Robert <tr@datak.fr>
2016-01-13 16:18:38 +0100

    AmTests
    AmTests

---
+++ R/amTests.R
@@ -0,0 +1,102 @@
+#Test if num is not numeric  or integer
+.testNumeric <- function(num, arg = NULL){
+  
+  if(is.null(arg))
+  {
+    arg <- substitute(num)
+  }
+  
+  if (!class(num) %in% c("numeric","integer"))
+  {
+    stop(paste0(arg," must be numeric, instead of ", class(num)))
+  }
+}
+
+#Test if num is not logical 
+.testLogical <- function(logi, arg = NULL){
+  if(is.null(arg))
+  {
+    arg <- substitute(logi)
+  }
+  if (!class(logi) %in% c("logical"))
+  {
+    stop(paste0(arg," must be logical, instead of ", class(logi)))
+  }
+}
+
+
+
+.testCharacter <- function(char, arg = NULL){
+  if(is.null(arg))
+  {
+    arg <- substitute(char)
+  }
+  if (!class(char) %in% c("character","factor"))
+  {
+    stop(paste0(arg," must be character, instead of ", class(char)))
+  }
+}
+
+
+#Test if num<binf or/and num>bsup
+.testInterval <- function(num, binf = NULL, bsup = NULL, arg = NULL){
+  if(is.null(arg))
+  {
+    arg <- substitute(num)
+  }
+  if (!is.null(binf))
+  {
+    if (num<binf)
+    {
+      stop(paste0(arg, " < ",binf, " must be upper"))
+    }
+  }
+  
+  if (!is.null(bsup))
+  {
+    if (num>bsup)
+    {
+      stop(paste0(arg, " > ",bsup, " must be lower"))
+    }
+  }
+}
+
+
+#Test if length(param)%in%len
+.testLength <- function(param, len = NULL, arg = NULL){
+  if(is.null(arg))
+  {
+    arg <- substitute(param)
+  }
+  if (!is.null(len)){
+    if (!length(param) %in% len){
+      stop(paste0("length of ", arg, " must be", ifelse(length(len)==1, " ", " in "),
+                  sprintf(as.character(len)), " currently ", length(param)))
+    }
+  }
+}
+
+#Test numeric and length 1
+.testNumericLength1 <- function(num){
+  arg <- substitute(num)
+  .testLength(param = num, len = 1, arg = arg)
+  .testNumeric(num = num, arg = arg)
+}
+
+#Test character and length 1
+.testCharacterLength1 <- function(char){
+  arg <- substitute(char)
+  .testLength(param = char, len = 1, arg = arg)
+  .testCharacter(char = char, arg = arg)
+}
+
+
+#Test logical and length 1
+.testestLogicalLength1 <- function(logi){
+  arg <- substitute(logi)
+  .testLength(param = logi, len = 1, arg = arg)
+  .testLogical(logi = logi, arg = arg)
+}
+
+
+
