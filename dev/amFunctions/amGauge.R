b351f3d2c3b6017a629b91d2d97d5192b780f297
Titouan Robert <tr@datak.fr>
2016-01-14 18:05:15 +0100

    Gauge new control and exemples
    Gauge new control and exemples

--- dev/amFunctions/amGauge.R
+++ dev/amFunctions/amGauge.R
@@ -8,36 +8,15 @@
 #' end (numeric, maximal value for the band), color (character, color of the ban, in hexadecimal)
 #' and width (numeric, width of the band). If the last column is not defined, it is auomatically
 #' set to 10. 
-#' @param main \code{character}, title of the graph.
-#' @param mainSize \code{numeric}, size of the title of the graph.
-#' @param legend \code{character} text lengend
-#' @param legendSize \code{numeric} text legend size
+#' @param text \code{character} text lengend
+#' @param textSize \code{numeric} text size
 #' @param secondAxe \code{boolean} TRUE if two axes are desired. Default is set to FALSE.
 #' @param start2 \code{numeric} minimum value allowed for the second axe if secondAxe is TRUE.
 #' @param end2 \code{numeric} maximum value allowed for the second axe if secondAxe is TRUE.
 #' @param step2 \code{numeric} intervals size for the second axe if secondAxe is TRUE.
 #' @param bands2 a list containing lists with bands informations for the second axe, named : bandStartValue (numeric),
 #' bandEndValue (numeric), bandColor (hexadecimal color value)
-#' @examples
-#' 
-#' #basic example
-#' amAngularGauge(x = 25)
-#' 
-#' #add colored bands
-#' bands = data.frame(start = c(0, 40, 60), end = c(40, 60, 100), 
-#'                    color = c("#00CC00", "#ffac29", "#ea3838"),
-#'                    stringsAsFactors = FALSE)
-#' amAngularGauge(x = 25, bands = bands)
-#'    
-#' #add some text to the displayed value
-#' amAngularGauge(x = 25, legend = "km/h")
-#' 
-#' #a gauge with two axis
-#' bands2 = data.frame(start = c(100, 130, 170), end = c(130, 170, 200), 
-#'                    color = c("#00CC00", "#ffac29", "#ea3838"),
-#'                    stringsAsFactors = FALSE)
-#' amAngularGauge(x = 25, start = 0, end = 100, bands = bands,
-#'                secondAxe = TRUE, start2 = 100, end2 = 200, bands2 = bands2)
+#' @examples ./examples/amGauge_examples.R
 #' 
 #' @import pipeR
 #' @export
@@ -46,102 +25,66 @@ amAngularGauge <- function(x, start = 0, end = 100, step = 20,
                            bands = data.frame(start = numeric(), end = numeric(),
                                                    color = character(), width = numeric(),
                                               stringsAsFactors = FALSE), 
-                           main = "", mainSize = 15, legend = "", 
-                           legendSize = 25, secondAxe = FALSE, start2 = 0, 
+                          text = "", 
+                          textSize = 25, secondAxe = FALSE, start2 = 0, 
                            end2 = 100, step2 = 20, 
                            bands2 = data.frame(start = numeric(), end = numeric(),
                                                     color = character(),
                                                stringsAsFactors = FALSE)) {
+  ##Test
+  #x
+  .testNumericLength1(x)
   
-  if(!is.numeric(x)) {
-    stop("x must be numeric")
-  } else {}
+  #start
+  .testNumeric(start)
   
-  if(!is.numeric(start)) {
-    stop("start must be numeric")
-  } else {}
+  #end
+  .testNumeric(end)
   
-  if(!is.numeric(end)) {
-    stop("end must be numeric")
-  } else {}
+  #step
+  .testNumeric(step)
   
-  if(!is.numeric(step)) {
-    stop("step must be numeric")
-  }
+  #good colanmes(bands)
+  .testIn("start",  colnames(bands))
+  .testIn("end",  colnames(bands))
+  .testIn("color",  colnames(bands))
   
-  if(!is.data.frame(bands) | !any(c("start", "end", "color") %in% colnames(bands))) {
-    stop ("bands must be a data frame which at least the columns 'start' (numeric),
-          'end' (numeric), and 'color' (chararcter, color in hexadecimal)")
-  } else {}
   
-  if(!is.numeric(bands$start)) {
-    stop("column 'start' of the dataframe bands must be numeric")
-  } else {}
+  #good format bands
+  .testNumeric(bands$start, arg = "bands$start")
+  .testNumeric(bands$end, arg = "bands$end")
+  .testCharacter(bands$color, arg = "bands$color")
   
-  if(!is.numeric(bands$end)) {
-    stop("column 'end' of the dataframe bands must be numeric")
-  } else {}
   
-  if(!is.character(bands$color)) {
-    stop("column 'color' of the dataframe bands must be character (color in hexadecimal)")
-  } else {}
+ 
+  text <- as.character(text)
   
-  main <- as.character(main)
+  #text size
+  .testNumeric(textSize)
+
+  .testLogicalLength1(secondAxe)
+
+  .testNumeric(start2)
+  .testNumeric(end2)
+  .testNumeric(step2)
+
   
-  if(!is.numeric(mainSize)) {
-    stop("mainSize must be numeric")
-  } else {}
+  #good colanmes(bands)
+  .testIn("start",  colnames(bands2))
+  .testIn("end",  colnames(bands2))
+  .testIn("color",  colnames(bands2))
   
-  legend <- as.character(legend)
   
-  if(!is.numeric(legendSize)) {
-    stop("legendSize must be numeric")
-  } else {}
+  #good format bands
+  .testNumeric(bands2$start, arg = "bands2$start")
+  .testNumeric(bands2$end, arg = "bands2$end")
+  .testCharacter(bands2$color, arg = "bands2$color")
   
-  if(!is.logical(secondAxe)) {
-    stop("secondAxe must be logical")
-  }
   
-  if(!is.numeric(start2)) {
-    stop("start2 must be numeric")
-  } else {}
+  .testInterval(start, bsup = end)
+  .testInterval(start2, bsup = end2)
   
-  if(!is.numeric(end2)) {
-    stop("end2 must be numeric")
-  } else {}
-  
-  if(!is.numeric(step2)) {
-    stop("step2 must be numeric")
-  }
-  
-  if(!is.data.frame(bands2) | !any(c("start", "end", "color") %in% colnames(bands2))) {
-    stop ("bands2 must be a data frame which at least the columns 'start' (numeric),
-          'end' (numeric), and 'color' (chararcter, color in hexadecimal)")
-  } else {}
-  
-  if(!is.numeric(bands2$start)) {
-    stop("column 'start' of the dataframe bands2 must be numeric")
-  } else {}
-  
-  if(!is.numeric(bands2$end)) {
-    stop("column 'end' of the dataframe bands2 must be numeric")
-  } else {}
-  
-  if(!is.character(bands2$color)) {
-    stop("column 'color' of the dataframe bands2 must be character (color in hexadecimal)")
-  } else {}
-  
-  if(start > end) {
-    stop("start as to be inferior to end")
-  } else {}
-  
-  if(start2 > end2) {
-    stop("start2 as to be inferior to end2")
-  } else {}
-  
-  if(x < start | x > end) {
-    stop("x must be between start and end")
-  }
+  .testInterval(x,binf = start, bsup = end)
   
   bands_1 <- list()
   
@@ -187,9 +130,8 @@ amAngularGauge <- function(x, start = 0, end = 100, step = 20,
   res <- pipeR::pipeline(
     amAngularGaugeChart(startValue = start, endValue = end, valueInterval = step),
     addArrow(value = x),
-    addTitle(text = main, size = mainSize),
     addAxis(startValue = start, endValue = end, valueInterval = step, bands = bands_1,
-            bottomText = paste(x, legend), bottomTextFontSize = legendSize, radius = radius1)
+            bottomText = paste(x, text), bottomTextFontSize = textSize, radius = radius1)
     
   )
   if(secondAxe == TRUE) {
@@ -212,75 +154,55 @@ amAngularGauge <- function(x, start = 0, end = 100, step = 20,
 #' @param x \code{integer} an integer of the value for which the solid gauge is desired.
 #' @param min \code{numeric} minimal value possible
 #' @param max \code{numeric} maximal value possible
-#' @param main \code{character}, title of the graph.
-#' @param mainSize \code{numeric}, size of the title of the graph.
 #' @param type \code{character} type of gauge : "full" or "semi"
 #' @param width \code{numeric} width of the gauge
 #' @param color \code{character} hexadecimal color value or a vector of colors
-#' @param legend \code{character} text legend
-#' @param legendSize \code{numeric} text legend size
-#' @examples
-#' amSolidGauge(x = 75, min = 0, max = 100, type = "full", width = 20, 
-#'              main = "Solid Gaude : full", color = "#1e90ff", legend = "%",
-#'              legendSize = 50)
-#' amSolidGauge(x = 70, min = 0, max = 100, type = "semi", width = 20, 
-#'              main = "Solid Gauge : semi", color = c("#00ff00", "#ffd700", "#ff0000"), legend = "%",
-#'              legendSize = 25)
+#' @param text \code{character} text
+#' @param textSize \code{numeric} text size
+#' @examples ./examples/amSolidGauge_examples.R
 #' @import data.table
 #' @export
 #' 
 amSolidGauge <- function(x, min = 0, max = 100, type = "full", width = 20,
-                           main = "", mainSize = 15, color = "", legend = "",
-                         legendSize = 20) {
+                         color = "", text = "",
+                         textSize = 20) {
   
   if (!requireNamespace(package = "pipeR")) {
     stop ("Please install the package pipeR for running this function")
   } else {}
   
-  if(!is.numeric(x)) {
-    stop("x must be numeric")
-  } else {}
   
-  if(!is.numeric(min)) {
-    stop("min must be numeric")
-  } else {}
+  #x
+  .testNumericLength1(x)
   
-  if(!is.numeric(max)) {
-    stop("max must be numeric")
-  } else {}
+  #min
+  .testNumeric(min)
   
-  if(!is.character(type) | !type %in% c("full", "semi")) {
-    stop("type must be a character, 'full' or 'semi'")
-  } else {}
+  #max
+  .testNumeric(max)
+
+  #type
+  .testCharacter(type)
+  .testIn(type, c("full", "semi"))
   
-  if(!is.numeric(width)) {
-    stop("width must be numeric")
-  } else {}
+  #width
+  .testNumeric(width)
+
+  #color
+  .testCharacter(color)
+
+
   
-  main = as.character(main)
+  text = as.character(text)
   
-  if(!is.numeric(mainSize)) {
-    stop("mainSize must be numeric")
-  } else {}
+  #textSize
+  .testNumericLength1(textSize)
+
+  #min, max, x
+  .testInterval(min, bsup = max)
+  .testInterval(x, binf = min, bsup = max)
   
-  if(!is.character(color)) {
-    stop("color must be a character (color in hexadecimal)")
-  } else {}
-  
-  legend = as.character(legend)
-  
-  if(!is.numeric(legendSize)) {
-    stop("legendSize must be numeric")
-  } else {}
-  
-  
-  if(min > max) {
-    stop("min as to be inferior to max")
-  } else {}
-  
-  if(x < min | x > max) {
-    stop("x must be between min and max")
-  }
+
   
   if(width < 100 & width > 0) {
     innerRadius <- paste0(100-width, "%")
@@ -317,10 +239,9 @@ amSolidGauge <- function(x, min = 0, max = 100, type = "full", width = 20,
   res <- pipeR::pipeline(
     amAngularGaugeChart(startValue = min, endValue = max, valueInterval = max,
                         labelsEnabled = FALSE),
-    addTitle(text = main, size = mainSize),
     addAxis(startAngle = startAngle, endAngle = endAngle,
             startValue = min, endValue = max, bands = bands,
-            bottomText = paste(x, legend), bottomTextYOffset = bottomTextYOffset, bottomTextFontSize = legendSize,
+            bottomText = paste(x, text), bottomTextYOffset = bottomTextYOffset, bottomTextFontSize = textSize,
             tickAlpha = 0, axisColor= "#c1cdcd", labelsEnabled = FALSE)
   )
   
