faef7f3098ba2f1de84a9f61bd5fa6b23a1966b2
Titouan Robert <tr@datak.fr>
2016-01-15 14:27:12 +0100

    Transform amMekko
    Transform amMekko

---
+++ R/amMekko.R
@@ -0,0 +1,90 @@
+#' @title Plotting mekko chart (quali vs quali) using rAmCharts
+#' @description  amMekko computes a mekko chart of the given values.
+#' @param x \code{character} column name for x-axis
+#' @param y \code{character} column name for y-axis
+#' @param data \code{data.frame} dataframe with values to display.
+#' @param groups_color \code{character} vector of colors in hexadecimal, 
+#' same length as the number of y modalities.
+#' @param xlab \code{character} label for x-axis.
+#' @param ylab \code{character} label for y-axis.
+#' @param horiz \code{boolean} TRUE for an horizontal chart, FALSE for a vertical one
+#' @param show_values \code{boolean} TRUE to display values.
+#' 
+#' @example examples/amMekko_examples.R
+#' 
+#' @export
+amMekko <- function(x, y, data, xlab = "", 
+                    ylab = "", groups_color = NULL,
+                    horiz = FALSE, show_values = FALSE) {
+  
+  
+  ##test
+  .testCharacterLength1(x)
+  .testCharacterLength1(y)
+  
+  .testIn(x, colnames(data))
+  .testIn(y, colnames(data))
+  #data
+  
+  
+  
+  data[,x] <- as.factor(as.character(data[,x]))
+  data[,y] <- as.factor(as.character(data[,y]))
+  
+  .testCharacterLength1(xlab)
+  .testCharacterLength1(ylab)
+  .testLogicalLength1(horiz)
+
+
+  .testLogicalLength1(show_values)
+  
+  
+  if(show_values) {
+    label_text <- "[[value]]"
+  } else {
+    label_text <- ""
+  }
+  
+  tab1 <- as.data.frame.matrix(table(data[,c(x,y)]))
+  tab1$total <- rowSums(tab1)
+  tab1$abs <- rownames(tab1)
+  
+  res <- pipeR::pipeline(
+    amSerialChart(dataProvider = tab1, categoryField = "abs", rotate = horiz,
+                  columnWidth = 1),
+    addValueAxis(title = ylab, position = 'left', stackType = "100% stacked",
+                 unit = "%", axisAlpha = 0, gridAlpha = 0.1),
+    setCategoryAxis(title = xlab, gridPosition = 'start', labelRotation = 45, 
+                    axisAlpha = 0, gridAlpha = 0, widthField = "total")
+  )
+  
+  if(!is.null(groups_color)) {
+    .testCharacter(groups_color)
+    if(length(groups_color) == length(y)) {
+      v_col <- groups_color
+    } else {
+      v_col <- head(rep(c("#67b7dc", "#fdd400", "#84b761", "#cc4748", 
+                          "#cd82ad", "#2f4074", "#448e4d", "#b7b83f", 
+                          "#b9783f", "#b93e3d", "#913167"), 5), 
+                    length(y))
+    }
+  } else {
+    v_col <- head(rep(c("#67b7dc", "#fdd400", "#84b761", "#cc4748", 
+                        "#cd82ad", "#2f4074", "#448e4d", "#b7b83f", 
+                        "#b9783f", "#b93e3d", "#913167"), 5), 
+                  length(y))
+  }
+  
+  sapply(1:(ncol(tab1)-2), FUN = function(i) {
+    valcol <- colnames(tab1)[i]
+    res <<- addGraph(res, id = paste0("AmGraph-",i),
+                     balloonText = paste0('<b>[[category]] - ',valcol,'</b> \n[[value]]'), fillColors = v_col[i], 
+                     legendColor = v_col[i],
+                     fillAlphas = 0.8, lineAlpha = 0, type = 'column', valueField = valcol,
+                     title = valcol, labelText = label_text)
+  })
+
+  res
+  
+}
+
---
+++ examples/amMekko_examples.R
@@ -0,0 +1,12 @@
+data_mekko <- data.frame(var1 = c(rep("A1", 150), rep("A2", 350), rep("A3", 500)),
+                         var2 = sample(c("B1", "B2", "B3", "B4", "B5", "B6"), 
+                                       1000, replace = TRUE))
+                                       
+amMekko(x = "var1", y = "var2", data = data_mekko)
+
+#Horizontal :
+amMekko(x = "var1", y = "var2", data = data_mekko, horiz = TRUE)
+
+#display values :
+amMekko(x = "var1", y = "var2", data = data_mekko, show_values = TRUE)
+
