0a71d3745fe82a92243b8313b0ea61b7f9d0d581
Jeffery Petit <jeff@datak.fr>
2016-02-10 12:37:08 +0100

    problem with export CSV for an amAngularGauge

--- issues/amOptions.R
+++ issues/amOptions.R
@@ -1,7 +1,7 @@
 require(pipeR)
+
+# This has been created to list the problem encountered with 'amOptions'
+
 # problem with export options
 amAngularGauge(x = 25) %>>% setExport()
-amAngularGauge(x = 25, exportFormat = "SVG")
-data("data_candleStick2")
-amCandlestick(data = data_candleStick2, horiz = TRUE, exportFormat = "CSV") %>>%
-  setProperties(precision = 5)
+amAngularGauge(x = 25, exportFormat = "CSV") # impossible
