139358ebe987d891fe5fdc76d3ecef24662792a7
Titouan Robert <tr@datak.fr>
2016-03-02 14:40:22 +0100

    Bug
    Bug

--- R/tools_amOptions.R
+++ R/tools_amOptions.R
@@ -118,23 +118,23 @@ amOptions <- function(chart, theme = "none", legend = FALSE, legendPosition = "r
             legend <- chart@dataProvider
             if(length(unique(unlist(legend)[names(unlist(legend)) == "color"])) > 1)
             {
-            print("Ok")
+         
             lapply(1:length(legend), function(X){
               names(legend[[X]])[names(legend[[X]]) == chart@categoryField] <<- "title"
             })
             
           
               out <<- legend
-              print(legend)
+           
 
               
               
               } else{
-              print(unique(unlist(legend)[names(unlist(legend)) == "color"]))
+             
               legend <- list(list(title = chart@categoryField,
                                   color = unique(unlist(legend)[names(unlist(legend)) == "color"])))
               out2 <<- legend
-              print(legend)
+            
             }
             
             chart <- setLegend(.Object = chart, data = legend)
