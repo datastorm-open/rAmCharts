cdcc97a9f34a6ca77557f8f0f327e89e488592d6
Jeffery Petit <jeff@datak.fr>
2016-03-16 12:36:44 +0100

    shiny examples

--- inst/shiny/src/lines/amLines_server.R
+++ inst/shiny/src/lines/amLines_server.R
@@ -1,15 +1,15 @@
 output$amlines0 <- rAmCharts::renderAmCharts({
-  ##Data
+  ## Data
   data('data_AirPassengers')
-  ##Plot
-  amPlot(AirPassengers ~ Period, data = data_AirPassengers, type = 'l')
+  ## Plot
+  amPlot(type = "l", AirPassengers ~ Period, data = data_AirPassengers)
 })
 
 output$code_amlines0 <- renderText({
   "
-  ##Data
+  ## Data
   data('data_AirPassengers')
-  ##Plot
+  ## Plot
   amPlot(AirPassengers ~ Period, data = data_AirPassengers, type = 'l')
   "
 })
@@ -17,23 +17,23 @@ output$code_amlines0 <- renderText({
 # ---
 
 output$amlines1 <- rAmCharts::renderAmCharts({
-  ##Data
+  ## Data
   start <- as.POSIXct('01-01-2015', format = '%d-%m-%Y')
   end <- as.POSIXct('31-12-2015', format = '%d-%m-%Y')
   date <- seq.POSIXt(from = start, to = end, by = 'day')
   date <- format(date, '%m-%d-%Y')
-  ##Plot
+  ## Plot
   amPlot(x = date, y = runif(365), type = 'l', parseDates = TRUE, dataDateFormat = 'MM-DD-YYYY')
 })
 
 output$code_amlines1 <- renderText({
   "
-  ##Data
+  ## Data
   start <- as.POSIXct('01-01-2015', format = '%d-%m-%Y')
   end <- as.POSIXct('31-12-2015', format = '%d-%m-%Y')
   date <- seq.POSIXt(from = start, to = end, by = 'day')
   date <- format(date, '%m-%d-%Y')
-  ##Plot
+  ## Plot
   amPlot(x = date, y = runif(365), type = 'l', parseDates = TRUE, dataDateFormat = 'MM-DD-YYYY')
   "
 })
@@ -41,12 +41,12 @@ output$code_amlines1 <- renderText({
 # ---
 
 output$amlines2 <- rAmCharts::renderAmCharts({
-  ##Data
+  ## Data
   start <- as.POSIXct('01-01-2015', format = '%d-%m-%Y')
   end <- as.POSIXct('11-04-2015', format = '%d-%m-%Y')
   date <- seq.POSIXt(from = start, to = end, by = 'day')
   date <- format(date, '%m-%d-%Y')
-  ##Plot
+  ## Plot
   pipeR::pipeline(
     amPlot(y = rnorm(100), type = 'sl', x = date, parseDates = TRUE, dataDateFormat = 'MM-DD-YYYY'),
     amLines(y = rnorm(100), type = 'p'),
@@ -56,12 +56,12 @@ output$amlines2 <- rAmCharts::renderAmCharts({
 
 output$code_amlines2 <- renderText({
   "
-  ##Data
+  ## Data
   start <- as.POSIXct('01-01-2015', format = '%d-%m-%Y')
   end <- as.POSIXct('11-04-2015', format = '%d-%m-%Y')
   date <- seq.POSIXt(from = start, to = end, by = 'day')
   date <- format(date, '%m-%d-%Y')
-  ##Plot
+  ## Plot
   pipeR::pipeline(
     amPlot(y = rnorm(100), type = 'sl', x = date, parseDates = TRUE, dataDateFormat = 'MM-DD-YYYY'),
     amLines(y = rnorm(100), type = 'p'),
