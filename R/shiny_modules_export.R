32c5b888de455e59ebd9d864e291e677f2619a34
B.Thieurmel <bthieurmel@gmail.com>
2017-01-18 14:25:57 +0100

    single or multiple export on module

--- R/shiny_modules_export.R
+++ R/shiny_modules_export.R
@@ -15,9 +15,10 @@
 #'  \item{"type"}{Optionnal, character. Linked to \link{amChartsOutput}}
 #'}
 #' @param path character, directory. tempdir() by Defaut
+#' @param mode character, 'single' : graphics are rendered and saved one by one. 'multiple' all at same time
 #' @param progress boolean, set a progress bar or not ?
-#' @param message charcter, if progress, message. Defaut to "Calculation in progress" 
-#' @param detail charcter, if progress, detail. Defaut to "This may take a while...'
+#' @param message character, if progress, message. Defaut to "Calculation in progress" 
+#' @param detail character, if progress, detail. Defaut to "This may take a while...'
 #' 
 #' @return a reactive expression
 #' 
@@ -60,9 +61,9 @@ rAmChartExportServerUI <- function(id) {
 
 #' @rdname rAmCharts-shinymodules
 #' @export
-rAmChartExportServer <- function(input, output, session, list_am_graph, 
-                                 path = shiny::reactive(tempdir()), progress = T, 
-                                 message = 'Calculation in progress', 
+rAmChartExportServer <- function(input, output, session, list_am_graph,
+                                 path = shiny::reactive(tempdir()), mode = "single", progress = T,
+                                 message = 'Calculation in progress',
                                  detail = 'This may take a while...'){
   ns <- session$ns
   
@@ -100,7 +101,11 @@ rAmChartExportServer <- function(input, output, session, list_am_graph,
   
   # for conditionnalPanel
   output$racems_n_g_evo <- shiny::reactive({
-    cpt$ind
+    if(mode == "single"){
+      cpt$ind
+    } else {
+      cpt$ind <= cpt$max
+    }
   })
   
   shiny::outputOptions(output, "racems_n_g_evo", suspendWhenHidden = FALSE)
@@ -137,7 +142,9 @@ rAmChartExportServer <- function(input, output, session, list_am_graph,
               type <- args_amco$type
             }
             
-            shiny::conditionalPanel(condition = paste0("output['", ns("racems_n_g_evo"), "'] == ", i),
+            shiny::conditionalPanel(condition = ifelse(mode == "single", 
+                                                       paste0("output['", ns("racems_n_g_evo"), "'] == ", i),
+                                                       paste0("output['", ns("racems_n_g_evo"), "']")),
                                     shiny::div(amChartsOutput(plotname, width = width, height = height, type = type), br(), align = "center")
             )
           })
@@ -149,78 +156,161 @@ rAmChartExportServer <- function(input, output, session, list_am_graph,
     }
   })
   
-  shiny::observe({
-    ind_chart <- cpt$ind
-    if(ind_chart != -1 & ind_chart <= cpt$max){
-      shiny::isolate({
-        plotname <- paste0("racems_graph_", ind_chart, "_", shiny::isolate(cpt$tmp))
-        output[[plotname]] <- renderAmCharts({
-          cur_am <- list_am_graph()[[ind_chart]]$graph
-          if(!is.null(cur_am)){
-            add_export <- F
-            if(!"export" %in% names(attr(cur_am, "otherProperties"))){
-              add_export <- T
-            } else {
-              if(!attr(cur_am, "otherProperties")$export$enabled){
+  if(mode == "single"){
+    shiny::observe({
+      ind_chart <- cpt$ind
+      if(ind_chart != -1 & ind_chart <= cpt$max){
+        shiny::isolate({
+          plotname <- paste0("racems_graph_", ind_chart, "_", shiny::isolate(cpt$tmp))
+          output[[plotname]] <- renderAmCharts({
+            cur_am <- list_am_graph()[[ind_chart]]$graph
+            if(!is.null(cur_am)){
+              add_export <- F
+              if(!"export" %in% names(attr(cur_am, "otherProperties"))){
                 add_export <- T
+              } else {
+                if(!attr(cur_am, "otherProperties")$export$enabled){
+                  add_export <- T
+                }
+              }
+              
+              if(add_export){
+                cur_am <- setExport(cur_am, enabled = TRUE, menu = list())
+              }
+              cur_am <- addListener(cur_am, name = "rendered", expression = paste0('function(e) {
+                                                                                   // WAIT FOR FABRIC
+                                                                                   var interval = setInterval( function() {
+                                                                                   if ( window.fabric ) {
+                                                                                   clearTimeout( interval );
+                                                                                   // CAPTURE CHART
+                                                                                   e.chart["export"].capture( {}, function() {
+                                                                                   // SAVE TO JPG
+                                                                                   this.toJPG( {}, function( base64 ) {
+                                                                                   // LOG IMAGE DATA
+                                                                                   Shiny.onInputChange("', ns("racems_img64"), '", base64);
+                                                                                   // clean
+                                                                                   e.chart.events.rendered = [];
+                                                                                   e.chart.clear();
+                                                                                   } );
+                                                                                   } );
+                                                                                   }
+                                                                                   }, 100 );}'))
+              cur_am
+            } else {
+              NULL
+            }
+          })
+        })
+      }
+    })
+    
+    shiny::observe({
+      if(!is.null(input$racems_img64)){
+        shiny::isolate({
+          if(input$racems_img64 != ""){
+            path_file <- paste0(path(), "/", list_am_graph()[[cpt$ind]]$name)
+            outconn <- file(path_file, "wb")
+            base64enc::base64decode(what=gsub("^data:image/jpeg;base64,", "", input$racems_img64), output = outconn)
+            close(outconn)
+            
+            # increase/close progress
+            progress_bar <- progress_bar()
+            if(!is.null(progress_bar)){
+              progress_bar$set(cpt$ind)
+              if(cpt$ind == cpt$max){
+                progress_bar$close()
               }
             }
-            
-            if(add_export){
-              cur_am <- setExport(cur_am, enabled = TRUE, menu = list())
-            }
-            cur_am <- addListener(cur_am, name = "rendered", expression = paste0('function(e) {
-                                                                               // WAIT FOR FABRIC
-                                                                               var interval = setInterval( function() {
-                                                                               if ( window.fabric ) {
-                                                                               clearTimeout( interval );
-                                                                               // CAPTURE CHART
-                                                                               e.chart["export"].capture( {}, function() {
-                                                                               // SAVE TO JPG
-                                                                               this.toJPG( {}, function( base64 ) {
-                                                                               // LOG IMAGE DATA
-                                                                               Shiny.onInputChange("', ns("racems_img64"), '", base64);
-                                                                               // clean
-                                                                               //e.chart.events.rendered = [];
-                                                                               //e.chart.clear();
-                                                                               //this.setup.chart.div.style = "display:none";
-                                                                               } );
-                                                                               } );
-                                                                               }
-                                                                               }, 100 );}'))
-            cur_am
-          } else {
-            NULL
+            cpt$ind <- cpt$ind +1
           }
         })
-      })
-    }
-  })
-  
-  shiny::observe({
-    if(!is.null(input$racems_img64)){
-      shiny::isolate({
-        if(input$racems_img64 != ""){
-          path_file <- paste0(path(), "/", list_am_graph()[[cpt$ind]]$name)
-          outconn <- file(path_file, "wb")
-          base64enc::base64decode(what=gsub("^data:image/jpeg;base64,", "", input$racems_img64), output = outconn)
-          close(outconn)
-          
-          # increase/close progress
-          progress_bar <- progress_bar()
-          if(!is.null(progress_bar)){
-            progress_bar$set(cpt$ind)
-            if(cpt$ind == cpt$max){
-              progress_bar$close()
-            }
-          }
-          cpt$ind <- cpt$ind +1
-        }
-      })
-      
-    }
-  })
-  
+        
+      }
+    })
+  } else {
+    shiny::observe({
+      tmp_cpt <- cpt$tmp
+      if(tmp_cpt > 0){
+        shiny::isolate({
+          list_chart <- list_am_graph()
+          render_plot <- lapply(1:length(list_chart), function(ind_chart) {
+            plotname <- paste0("racems_graph_", ind_chart, "_", shiny::isolate(cpt$tmp))
+            output[[plotname]] <- renderAmCharts({
+              cur_am <- list_chart[[ind_chart]]$graph
+              if(!is.null(cur_am)){
+                add_export <- F
+                if(!"export" %in% names(attr(cur_am, "otherProperties"))){
+                  add_export <- T
+                } else {
+                  if(!attr(cur_am, "otherProperties")$export$enabled){
+                    add_export <- T
+                  }
+                }
+                
+                if(add_export){
+                  cur_am <- setExport(cur_am, enabled = TRUE, menu = list())
+                }
+                cur_am <- addListener(cur_am, name = "rendered", expression = paste0('function(e) {
+                                                                                     // WAIT FOR FABRIC
+                                                                                     var interval = setInterval( function() {
+                                                                                     if ( window.fabric ) {
+                                                                                     clearTimeout( interval );
+                                                                                     // CAPTURE CHART
+                                                                                     e.chart["export"].capture( {}, function() {
+                                                                                     // SAVE TO JPG
+                                                                                     this.toJPG( {}, function( base64 ) {
+                                                                                     // LOG IMAGE DATA
+                                                                                     Shiny.onInputChange("', ns(paste0("racems_img64_", ind_chart, "_",  shiny::isolate(cpt$tmp))), '", base64);
+                                                                                     // clean
+                                                                                     e.chart.events.rendered = [];
+                                                                                     e.chart.clear();
+                                                                                     // remove previous input
+                                                                                      Shiny.onInputChange("', ns(paste0("racems_img64_", ind_chart, "_",  shiny::isolate(cpt$tmp)-1)), '", null);
+                                                                                     } );
+                                                                                     } );
+                                                                                     }
+                                                                                     }, 100 );}'))
+                
+                
+                cur_am
+              } else {
+                NULL
+              }
+            })
+            
+            output[[paste0("save_graph_", ind_chart)]] <- reactive({
+              img64name <- paste0("racems_img64_", ind_chart, "_", shiny::isolate(cpt$tmp))
+              if(!is.null(input[[img64name]])){
+                if(input[[img64name]] != ""){
+                  isolate({
+                    
+                    path_file <- paste0(path(), "/", list_chart[[ind_chart]]$name)
+                    outconn <- file(path_file, "wb")
+                    base64enc::base64decode(what=gsub("^data:image/jpeg;base64,", "", input[[img64name]]), output = outconn)
+                    close(outconn)
+                    
+                    # increase/close progress
+                    progress_bar <- progress_bar()
+                    if(!is.null(progress_bar)){
+                      progress_bar$set(ind_chart)
+                      if(cpt$ind == cpt$max){
+                        progress_bar$close()
+                      }
+                    }
+                    cpt$ind <- cpt$ind +1
+                  })
+                }
+              }
+              NULL
+            })
+            
+            outputOptions(output, paste0("save_graph_", ind_chart), suspendWhenHidden = FALSE)  
+          })
+        })
+      }
+    })
+  }
+
   info <- reactive({
     cur_g <- cpt$ind-1
     max_g <- cpt$max
